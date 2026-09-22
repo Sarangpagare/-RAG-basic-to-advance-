cf28f553cb69c153df3aa04eed3d56a37c5ff685
Sarangpagare <sarangpagare612@gmail.com>
2026-09-23 03:20:26 +0530

    completing ingestion pipeline code

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -10,6 +10,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+# ::This is part of the ingestion pipeline, which is responsible for loading the documents from the docs directory and returning them as a list of Document objects.
 def load_documents(docs_path="docs"):
     """load all the text files from docs directory"""
     print(f"loading documents from {docs_path}...")
@@ -21,9 +23,11 @@ def load_documents(docs_path="docs"):
     #load all the text files from the docs directory
     loader = DirectoryLoader(
         path=docs_path,
-        glob="*.txt",
-        loader_cls=TextLoader  
+        glob="*.txt",   
+        loader_cls=TextLoader,  
+        loader_kwargs={"encoding": "utf-8"}
     )
+    
 
 
     documents = loader.load()
@@ -39,6 +43,53 @@ def load_documents(docs_path="docs"):
 
     return documents
 
+# ::Here we will do the Chunking part of the Documents  
+def split_documents(documents , chunk_size=1000, chunk_overlap=0):
+    '''splits document into smaller chunks with overlap '''  
+    print("Splitting documents into chunks...")
+
+    text_splitter = CharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap
+    )
+
+    chunks = text_splitter.split_documents(documents)
+
+    if chunks:
+
+        for i, chunk in enumerate(chunks[:5]):
+            print(f"\n---chunk{i+1} ---")
+            print(f"Source: {chunk.metadata['source']}")
+            print(f"Length: {len(chunk.page_content)}character")
+            print(f"Content: ")
+            print(chunk.page_content)
+            print("-" * 50)
+
+        if len(chunks) > 5:
+            print(f"\n... and {len(chunks) - 5} more chunks")
+
+    return chunks  
+
+#here we will do vector embeddings and storing it into vector database
+def create_vector_store(chunks, persist_directory="db/chroma_db"):
+    '''create and persist ChromaDB vector store'''
+    print("creating embedding and storing it in ChromaDB...")
+
+    embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+
+    #create Chroma vector store
+    print("--- Creating vector store ---")
+    vectorstore = Chroma.from_documents(
+        documents = chunks,
+        embedding = embedding_model,
+        persist_directory = persist_directory,
+        collection_metadata = { "hnsw:space", "cosine" }
+    )  
+    print("--- finished creating vector store---")
+
+    print(f"vector store craeted and saved to {persist_directory}")  
+    return vectorstore 
+   
 
 
 def main():
@@ -46,8 +97,15 @@ def main():
 
     #1. Load documents
     documents = load_documents(docs_path="docs")
+
     #2. Chunking the files
+    chunks = split_documents(documents)
+
     #3. create embeddings and storing in vector DB
+    vectorstore = create_vector_store(chunks)
+
+
+
 if __name__ == "__main__":
     main()
     
